4e6739f0f3a262de4038c8acdc46ef5b2e827065
Kenjin32icon <kariukilewis04@gmail.com>
2026-03-26 11:33:31 +0300

    feat: update Firebase configuration to include authentication and export necessary functions

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider } from "firebase/auth"; // MUST IMPORT THESE
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyClUR-F_lxtqJ53sKrEoeVnAoaGRv-OoQg",
   authDomain: "tuk-mapping-auth.firebaseapp.com",
@@ -19,3 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// THESE THREE LINES ARE MANDATORY FOR LOGIN TO WORK:
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
